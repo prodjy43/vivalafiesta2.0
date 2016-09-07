af21421cc8277eb714f26af71ca83dfbd9798818
prodjy43 <prodjy43@gmail.com>
2016-09-07 18:09:38 +0200

    last upadte

---
+++ resources/views/admin/editUser.blade.php
@@ -0,0 +1,35 @@
+@extends('default')
+
+@section('content')
+	<div class="wrapper style2">
+		<div class="title">{{ $title }}</div>
+		<div id="highlights" class="container">
+			{!! Form::model($user, ['url' => 'account/edit/'.$user->id, 'method' => 'put']) !!}
+				{!! Form::token() !!}
+				<div class="row 150%">
+					<div class="6u 12u(mobile)">
+						{!! Form::text('nom', null, $attributes = ['placeholder' => 'Nom', 'disabled' => 'disabled']) !!}
+					</div>
+					<div class="6u 12u(mobile)">
+						{!! Form::text('prenom', null, $attributes = ['placeholder' => 'Prenom', 'disabled' => 'disabled']) !!}
+					</div>
+					<div class="12u 12u(mobile)">
+						{!! Form::text('pseudo', null, $attributes = ['placeholder' => 'Pseudo', 'disabled' => 'disabled']) !!}
+					</div>
+					<div class="12u 12u(mobile)">
+						{!! Form::email('email', null, $attributes = ['placeholder' => 'Adresse E-mail', 'disabled' => 'disabled']) !!}
+					</div>
+					<div class="12u 12u(mobile)">
+						{!! Form::textarea('statut', null, $attributes = ['placeholder' => 'Statut de profil', 'disabled' => 'disabled']) !!}
+					</div>
+					<div class="12u 12u(mobile)">
+						{!! Form::select('grade', $grades) !!}
+					</div>
+					<div class="12u 12u(mobile)">
+						{!! Form::submit('Enregistrer', ['class' => 'style3']) !!}
+					</div>
+				</div>
+			{!! Form::close() !!}
+		</div>
+	</div>
+@endsection
